de03b3e5badaa2a5e47697429bbe529c34ebbed9
a.krantz <a.krantz@peak-solution.de>
2024-08-19 07:49:08 +0000

    fix test

--- tests/test_exd_api.py
+++ tests/test_exd_api.py
@@ -4,5 +4,5 @@ import asam_odsbox.proto.ods_external_data_pb2 as exd_data
 
 
 def test_strcuture_creation():
-    structure_request = exd_data.StructureRequest(handle=exd_api.Handle(uuid="abc-def"))
+    structure_request = exd_data.StructureRequest(handle=exd_data.Handle(uuid="abc-def"))
     assert structure_request is not None
