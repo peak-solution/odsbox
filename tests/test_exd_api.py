f35f6d3716c4a2d29a631c28de1197a6173484c5
a.krantz <a.krantz@peak-solution.de>
2024-08-19 07:47:02 +0000

    rename to exd-data

--- tests/test_exd_api.py
+++ tests/test_exd_api.py
@@ -1,8 +1,8 @@
 """Check if import of ext data works"""
 
-import asam_odsbox.proto.ods_external_data_pb2 as exd_api
+import asam_odsbox.proto.ods_external_data_pb2 as exd_data
 
 
 def test_strcuture_creation():
-    structure_request = exd_api.StructureRequest(handle=exd_api.Handle(uuid="abc-def"))
+    structure_request = exd_data.StructureRequest(handle=exd_api.Handle(uuid="abc-def"))
     assert structure_request is not None
