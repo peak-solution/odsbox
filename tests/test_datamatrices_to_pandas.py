ba2bb1a6a950a0b8f29ba4bb8aa95b70b66e0e6f
a.krantz <a.krantz@peak-solution.de>
2024-08-29 16:07:31 +0000

    DS values need to be converted to list

--- tests/test_datamatrices_to_pandas.py
+++ tests/test_datamatrices_to_pandas.py
@@ -1,4 +1,5 @@
 import logging
+import pytest
 
 import odsbox.proto.ods_pb2 as ods
 from odsbox.datamatrices_to_pandas import to_pandas
@@ -45,3 +46,80 @@ def test_all_types():
     pdf = to_pandas(dms)
     logging.getLogger().info(pdf)
     assert pdf.shape == (2, 7)
+    assert pdf.to_dict() == {
+        "AllTypes.Str": {0: "StrVal", 1: "StrVal"},
+        "AllTypes.I32": {0: 1, 1: 1},
+        "AllTypes.F32": {0: 1.2000000476837158, 1: 1.2000000476837158},
+        "AllTypes.Bool": {0: True, 1: True},
+        "AllTypes.U08": {0: 97, 1: 98},
+        "AllTypes.F64": {0: 2.2, 1: 2.2},
+        "AllTypes.I64": {0: 1, 1: 1},
+    }
+    assert len(pdf.to_json()) > 0
+
+
+def test_all_ds_types():
+    dms = ods.DataMatrices()
+    dm = dms.matrices.add(aid=4711, name="AllTypes")
+    values = dm.columns.add(name="DS_STRING", data_type=ods.DS_STRING).string_arrays.values
+    values.add().values.extend(["StrVal1"] * 3)
+    values.add().values.extend(["StrVal2"] * 2)
+    values = dm.columns.add(name="DS_LONG", data_type=ods.DS_LONG).long_arrays.values
+    values.add().values.extend([1] * 3)
+    values.add().values.extend([-1] * 2)
+    values = dm.columns.add(name="DS_FLOAT", data_type=ods.DS_FLOAT).float_arrays.values
+    values.add().values.extend([1.1] * 3)
+    values.add().values.extend([-1.1] * 2)
+    values = dm.columns.add(name="DS_BOOLEAN", data_type=ods.DS_BOOLEAN).boolean_arrays.values
+    values.add().values.extend([True] * 3)
+    values.add().values.extend([False] * 2)
+    values = dm.columns.add(name="DS_BYTE", data_type=ods.DS_BYTE).byte_arrays.values
+    values.add().values = b"abc"
+    values.add().values = b"de"
+    values = dm.columns.add(name="DS_DOUBLE", data_type=ods.DS_DOUBLE).double_arrays.values
+    values.add().values.extend([2.1] * 3)
+    values.add().values.extend([-2.1] * 2)
+    values = dm.columns.add(name="DS_LONGLONG", data_type=ods.DS_LONGLONG).longlong_arrays.values
+    values.add().values.extend([1234] * 3)
+    values.add().values.extend([-1234] * 2)
+
+    pdf = to_pandas(dms)
+    logging.getLogger().info(pdf)
+    assert pdf.shape == (2, 7)
+
+    assert pdf.to_dict() == {
+        "AllTypes.DS_STRING": {0: ["StrVal1", "StrVal1", "StrVal1"], 1: ["StrVal2", "StrVal2"]},
+        "AllTypes.DS_LONG": {0: [1, 1, 1], 1: [-1, -1]},
+        "AllTypes.DS_FLOAT": {
+            0: [1.100000023841858, 1.100000023841858, 1.100000023841858],
+            1: [-1.100000023841858, -1.100000023841858],
+        },
+        "AllTypes.DS_BOOLEAN": {0: [True, True, True], 1: [False, False]},
+        "AllTypes.DS_BYTE": {0: [97, 98, 99], 1: [100, 101]},
+        "AllTypes.DS_DOUBLE": {0: [2.1, 2.1, 2.1], 1: [-2.1, -2.1]},
+        "AllTypes.DS_LONGLONG": {0: [1234, 1234, 1234], 1: [-1234, -1234]},
+    }
+    assert len(pdf.to_json()) > 0
+
+
+def test_empty_datamatrices():
+    dms = ods.DataMatrices()
+    pdf = to_pandas(dms)
+    assert pdf.shape == (0, 0)
+
+
+def test_empty_datamatrix():
+    dms = ods.DataMatrices()
+    dms.matrices.add(aid=4711, name="AllTypes")
+    pdf = to_pandas(dms)
+    assert pdf.shape == (0, 0)
+
+
+def test_unsupported_types():
+    dms = ods.DataMatrices()
+    dm = dms.matrices.add(aid=4711, name="Unsupported")
+    dm.columns.add(name="DS_UNKNOWN", data_type=ods.DT_UNKNOWN).unknown_arrays.values.add(
+        data_type=ods.DT_DOUBLE, unit_id=12
+    )
+    with pytest.raises(ValueError, match="DataType 'unknown_arrays' not handled!"):
+        to_pandas(dms)
