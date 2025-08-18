56d08e07a848b5b0b7380dfd88a06322c87092f1
Andreas K <totonga@gmail.com>
2025-08-18 15:13:12 +0000

    integration tests

--- tests/test_bulk_reader_integration.py
+++ tests/test_bulk_reader_integration.py
@@ -4,6 +4,7 @@ import logging
 
 import pandas as pd
 from odsbox.con_i import ConI
+import pytest
 
 
 def __create_con_i(load_model: bool = True) -> ConI:
@@ -11,7 +12,7 @@ def __create_con_i(load_model: bool = True) -> ConI:
     return ConI("https://docker.peak-solution.de:10032/api", ("Demo", "mdm"), load_model=load_model)
 
 
-# @pytest.mark.integration
+@pytest.mark.integration
 def test_bulk_reader_simple():
     with __create_con_i() as con_i:
 
@@ -29,7 +30,9 @@ def test_bulk_reader_simple():
             submatrix_id = int(sm_s.iloc[0, 0])  # type: ignore
 
             df1 = con_i.bulk.data_read(submatrix_id, ["Time", "Coolant"], set_independent_as_index=False)
+            assert df1.empty is False
             df2 = con_i.bulk.valuematrix_read(submatrix_id, ["Time", "Coolant"])
+            assert df2.empty is False
             pd.testing.assert_frame_equal(df1, df2)
 
             pd.testing.assert_frame_equal(
@@ -44,3 +47,25 @@ def test_bulk_reader_simple():
             pd.testing.assert_frame_equal(
                 df1.set_index("Time"), con_i.bulk.data_read(submatrix_id, ["t?me", "coola?t"], True)
             )
+
+
+@pytest.mark.integration
+def test_bulk_reader_query():
+    with __create_con_i() as con_i:
+
+        conditions = {"submatrix.measurement.name": {"$like": "Profile_5?"}}
+        con_i.bulk.add_column_filters(conditions, ["Time", "Coolant"], column_patterns_case_insensitive=False)
+
+        df1 = con_i.bulk.query(conditions)
+        assert df1.empty is False
+        assert list(df1.columns[:4]) == ["submatrix", "name", "id", "values"]
+
+        submatrices = df1.groupby("submatrix")
+        assert len(submatrices) > 0
+        for _, group in submatrices:
+            s = group.set_index("name")["values"]
+            time_vals = s["Time"]
+            coolant_vals = s["Coolant"]
+            assert len(time_vals) > 0
+            assert len(coolant_vals) > 0
+            assert len(time_vals) == len(coolant_vals)
