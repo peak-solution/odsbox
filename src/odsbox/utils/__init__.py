bd42d7c98357e2fb3f6d9509081b1dbe6ba5bf93
a.krantz <a.krantz@peak-solution.de>
2026-09-21 00:31:39 +0200

    feat: add OdsUnitConverter and related documentation

---
+++ src/odsbox/utils/__init__.py
@@ -0,0 +1,4 @@
+from .context import from_context_variables, to_context_variables
+from .ods_unit_converter import OdsUnitConverter
+
+__all__ = ["OdsUnitConverter", "from_context_variables", "to_context_variables"]
